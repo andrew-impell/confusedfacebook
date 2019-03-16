1da76bbc93807e9616c5d7b9c6adb5d4e1db10e5
andrew-impell <andrew-impell@users.noreply.github.com>
2019-03-16 12:27:42 -0400

    Update confusedfacebook.py
    Added todo

--- confusedfacebook.py
+++ confusedfacebook.py
@@ -3,6 +3,14 @@ This is a program that creates a string to post to facebook using
 popular keywords for that time period to obsfucate the user's
 actual actions/views/etc and make them appear to be like another sheep in the
 herd
+
+TODO:
+
+Add a bash script to run file and send output to a txt file which 
+will be run weekly with a cron job and push a reminder using echo
+to remind to post
+
+
 """
 
 from bs4 import BeautifulSoup
